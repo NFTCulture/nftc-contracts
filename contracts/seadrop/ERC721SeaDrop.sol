50ce13ef639735d0295deaaec68250698cfd4032
Michael Villere <michael.villere@gmail.com>
2024-03-13 11:07:47 -0500

    make evm versions consistent

--- contracts/seadrop/ERC721SeaDrop.sol
+++ contracts/seadrop/ERC721SeaDrop.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.21;
 
 import {ERC721ContractMetadata, ISeaDropTokenContractMetadata} from './ERC721ContractMetadata.sol';
 
--- contracts/seadrop/interfaces/ISeaDrop.sol
+++ contracts/seadrop/interfaces/ISeaDrop.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.21;
 
 import {AllowListData, MintParams, PublicDrop, TokenGatedDropStage, TokenGatedMintParams, SignedMintValidationParams} from '../libs/SeaDropStructs.sol';
 
